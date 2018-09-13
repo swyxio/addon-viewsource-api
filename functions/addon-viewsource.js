f7d89b1f5feea2d4a2640159a40c6ff196374f9a
sw-yx <swyx@swyx.io>
2018-09-14 06:32:37 +0800

    init

---
+++ functions/addon-viewsource.js
@@ -0,0 +1,17 @@
+exports.handler = function(event, context, callback) {
+  // your server-side functionality
+
+  // {
+  //   "path": "Path parameter",
+  //   "httpMethod": "Incoming request's method name"
+  //   "headers": {Incoming request headers}
+  //   "queryStringParameters": {query string parameters }
+  //   "body": "A JSON string of the request payload."
+  //   "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+  // }
+
+  callback(null, {
+    statusCode: 200,
+    body: "Hello, World"
+  });
+};
