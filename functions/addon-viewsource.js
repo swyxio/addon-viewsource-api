de92f5b02d270f27c6e678cad44c931767905cd0
sw-yx <swyx@swyx.io>
2018-09-14 06:47:26 +0800

    hello world

--- functions/addon-viewsource.js
+++ functions/addon-viewsource.js
@@ -13,6 +13,6 @@ exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: "hello world"
+    body: JSON.stringify({ hello: "world" })
   });
 };
--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,6 @@ exports.handler = function(event, context, callback) {
   callback(null, {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: "hello world"
+    body: JSON.stringify({ hello: "world" })
   });
 };
