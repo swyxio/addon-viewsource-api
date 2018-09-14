68151c4e112747eea7f5b450f138867beaf2643d
sw-yx <swyx@swyx.io>
2018-09-14 15:22:54 +0800

    export funciton handler

--- lambdas/addon-viewsource.js
+++ lambdas/addon-viewsource.js
@@ -1,18 +1,29 @@
-exports.handler = function(event, context, callback) {
-  // your server-side functionality
+// exports.handler = function(event, context, callback) {
+//   // your server-side functionality
 
-  // {
-  //   "path": "Path parameter",
-  //   "httpMethod": "Incoming request's method name"
-  //   "headers": {Incoming request headers}
-  //   "queryStringParameters": {query string parameters }
-  //   "body": "A JSON string of the request payload."
-  //   "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
-  // }
+//   // {
+//   //   "path": "Path parameter",
+//   //   "httpMethod": "Incoming request's method name"
+//   //   "headers": {Incoming request headers}
+//   //   "queryStringParameters": {query string parameters }
+//   //   "body": "A JSON string of the request payload."
+//   //   "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
+//   // }
 
+//   callback(null, {
+//     statusCode: 200,
+//     headers: { "Content-Type": "application/json" },
+//     body: JSON.stringify({ hello: "world" })
+//   });
+// };
+const test = {
+  foo: "bar",
+  bar: 1
+};
+export function handler(event, context, callback) {
+  console.log(event);
   callback(null, {
     statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ hello: "world" })
+    body: JSON.stringify({ msg: "Hello, World!", test })
   });
-};
+}
