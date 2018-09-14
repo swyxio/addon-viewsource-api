f92ac0b541116e09090bc3fbf0d27949a9392204
sw-yx <swyx@swyx.io>
2018-09-14 14:36:05 +0800

    functions work

---
+++ lambdas/hello.js
@@ -0,0 +1,7 @@
+exports.handler = function(event, context, callback) {
+  callback(null, {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ hello: "world" })
+  });
+};
